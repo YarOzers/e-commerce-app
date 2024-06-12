d3fab1193ebd2b2f6185341c94cd988ce6327e45
Yaroslav Ozerskiy <overskiy.yaroslav@gmail.com>
2024-06-12 23:20:44 +0400

    implemented customer service

---
+++ services/customer/src/main/java/com/yaroslav/ecommerce/customer/Customer.java
@@ -0,0 +1,23 @@
+package com.yaroslav.ecommerce.customer;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.lang.annotation.Documented;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Customer {
+
+    @Id
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Address address;
+}
